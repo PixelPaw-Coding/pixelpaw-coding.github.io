17aa23a15a717081de4b41168562a7201632be2d
PixelPaw Coding <pixelpaw-coding@keemail.me>
2025-06-28 17:19:33 +0700

    Update packager.php

--- packager.php
+++ packager.php
@@ -1,33 +1,29 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['sb3'])) {
-    $tmp = $_FILES['sb3']['tmp_name'];
-    $filename = 'project_' . time() . '.zip';
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['sb3'])) {
+    $tmpName = $_FILES['sb3']['tmp_name'];
+    $outputZip = 'project_' . time() . '.zip';
 
     $zip = new ZipArchive;
-    if ($zip->open($filename, ZipArchive::CREATE) === TRUE) {
-        $zip->addFile($tmp, 'project.sb3');
+    if ($zip->open($outputZip, ZipArchive::CREATE) === TRUE) {
+        $zip->addFile($tmpName, 'project.sb3');
         $zip->close();
-
         header('Content-Type: application/zip');
-        header('Content-Disposition: attachment; filename="' . $filename . '"');
-        readfile($filename);
-        unlink($filename);
+        header('Content-Disposition: attachment; filename="' . $outputZip . '"');
+        readfile($outputZip);
+        unlink($outputZip);
         exit;
-    } else {
-        echo "Failed to create zip file.";
     }
 }
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Scratch SB3 Packager</title>
-</head>
+<head><title>Scratch Packager</title></head>
 <body>
-    <h2>Upload your .sb3 file to package it as a .zip</h2>
-    <form method="POST" enctype="multipart/form-data">
-        <input type="file" name="sb3" accept=".sb3" required>
-        <button type="submit">Create ZIP</button>
-    </form>
+<h2>Scratch Project ZIP Packager</h2>
+<form method="post" enctype="multipart/form-data">
+  <input type="file" name="sb3" accept=".sb3" required />
+  <br><br>
+  <button type="submit">Download ZIP</button>
+</form>
 </body>
 </html>
