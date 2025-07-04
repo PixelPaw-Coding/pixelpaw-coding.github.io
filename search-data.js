539dba33292761702ae9531f80cf5be912b2a62f
PixelPaw Coding <pixelpaw-coding@keemail.me>
2025-07-04 21:01:21 +0700

    Create search-data.js

---
+++ search-data.js
@@ -0,0 +1,17 @@
+const searchData = [
+  {
+    title: "Home",
+    url: "index.html",
+    content: "Welcome to PixelSearch. This is your home page."
+  },
+  {
+    title: "Projects",
+    url: "projects.html",
+    content: "Browse coding projects including JavaScript, HTML, and CSS."
+  },
+  {
+    title: "About",
+    url: "about.html",
+    content: "PixelSearch is a simple search engine made by PixelPaw."
+  }
+];
